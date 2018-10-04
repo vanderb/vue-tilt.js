2422b5e0bc97d314f1a6ba6fc29573639ff44c13
Freerk van Zeijl <freerk@Freerks-iMac.local>
2018-10-04 16:50:54 +0200

    Added Custom event polyfill for IE 11 (>= 9 really)

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [1.0.1] - 2018-10-04
+
+### Changed
+- Added Custom event polyfill for IE 11 (>= 9 really) [@vanderb](https://github.com/vanderb).
+
+## [1.0.0] - 2017-10-04
+### Added
+- Initial commit of the package [@vanderb](https://github.com/vanderb).
--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
-window._ = require('lodash');
+/*!
+ * ------------------------------
+ * Vue-Tilt.js - Based on Vanilla-Tilt ()
+ * https://github.com/frogbob/vue-tilt.js
+ * license: MIT license (http://opensource.org/licenses/MIT)
+ * ------------------------------
+ */
 
+
+/**
+ * Custom event polyfill for IE 11 (>= 9 really)
+ * Source: https://gist.github.com/gt3/787767e8cbf0451716a189cdcb2a0d08
+ */
+;(function() {
+    if (typeof window.CustomEvent === "function") return false
+  
+    function CustomEvent(event, params) {
+      params = params || { bubbles: false, cancelable: false, detail: undefined }
+      var evt = document.createEvent("CustomEvent")
+      evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail)
+      return evt
+    }
+  
+    CustomEvent.prototype = window.Event.prototype
+  
+    window.CustomEvent = CustomEvent
+})()
+
+// Import VanillaTilt
 import VanillaTilt from 'vanilla-tilt';
         
-const VueTilt = {
+// Define VueTilt-Directive
+export const VueTilt = {
 
-    install(Vue) {
-                
+    install(Vue) {  
+
         Vue.directive('tilt', (el, binding) => {
-            let options = {};
-            _.merge(options, binding.value);
-            VanillaTilt.init(el, options);
+            VanillaTilt.init(el, Object.assign({}, binding.value) );
         });
-        
+
     }
 
-}
-
-export default VueTilt;
+};
