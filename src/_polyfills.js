0649311eafc5c44f88fc922460f5b9c05eb90f76
Freerk van Zeijl <freerk@Freerks-iMac.local>
2018-10-04 17:02:38 +0200

    Added Object.assign polyfill for IE 11

---
+++ src/_polyfills.js
@@ -0,0 +1,54 @@
+/**
+ * Custom event polyfill for IE 11 (>= 9 really)
+ * Source: https://gist.github.com/gt3/787767e8cbf0451716a189cdcb2a0d08
+ */
+;(function() {
+  if (typeof window.CustomEvent === "function") return false
+
+  function CustomEvent(event, params) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined }
+    var evt = document.createEvent("CustomEvent")
+    evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail)
+    return evt
+  }
+
+  CustomEvent.prototype = window.Event.prototype
+
+  window.CustomEvent = CustomEvent
+})()
+
+/**
+ * Object.assign polyfill for IE 11
+ * Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
+ */
+;(function() {
+  if (typeof Object.assign != 'function') {
+    // Must be writable: true, enumerable: false, configurable: true
+    Object.defineProperty(Object, "assign", {
+      value: function assign(target, varArgs) { // .length of function is 2
+        'use strict';
+        if (target == null) { // TypeError if undefined or null
+          throw new TypeError('Cannot convert undefined or null to object');
+        }
+  
+        var to = Object(target);
+  
+        for (var index = 1; index < arguments.length; index++) {
+          var nextSource = arguments[index];
+  
+          if (nextSource != null) { // Skip over if undefined or null
+            for (var nextKey in nextSource) {
+              // Avoid bugs when hasOwnProperty is shadowed
+              if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                to[nextKey] = nextSource[nextKey];
+              }
+            }
+          }
+        }
+        return to;
+      },
+      writable: true,
+      configurable: true
+    });
+  }
+})()
--- src/index.js
+++ src/index.js
@@ -6,38 +6,22 @@
  * ------------------------------
  */
 
-
-/**
- * Custom event polyfill for IE 11 (>= 9 really)
- * Source: https://gist.github.com/gt3/787767e8cbf0451716a189cdcb2a0d08
- */
-;(function() {
-    if (typeof window.CustomEvent === "function") return false
-  
-    function CustomEvent(event, params) {
-      params = params || { bubbles: false, cancelable: false, detail: undefined }
-      var evt = document.createEvent("CustomEvent")
-      evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail)
-      return evt
-    }
-  
-    CustomEvent.prototype = window.Event.prototype
-  
-    window.CustomEvent = CustomEvent
-})()
+require('./_polyfills')
 
 // Import VanillaTilt
 import VanillaTilt from 'vanilla-tilt';
         
 // Define VueTilt-Directive
-export const VueTilt = {
+const VueTilt = {
 
-    install(Vue) {  
+    install: function(Vue) {  
 
-        Vue.directive('tilt', (el, binding) => {
+        Vue.directive('tilt', function (el, binding) {
             VanillaTilt.init(el, Object.assign({}, binding.value) );
         });
 
     }
 
 };
+
+export default VueTilt;
